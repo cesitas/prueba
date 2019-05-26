5140016d97d5bf4bc2220d9eac4b5e9e1dafa410
César Javier García - Páramo <cjgarcia@krama.es>
2019-05-26 21:07:15 +0200

    Lista de feeds y página de detalles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FeedProvider } from '../../providers/feed/feed.provider';
+import { FeedItemModel } from '../../models/feed.model';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    feedItems = [];
 
-  }
+    constructor(
+        public navCtrl: NavController,
+        private feedProvider: FeedProvider
+    ) {
+        this.feedProvider.loadFeed().then(feedItems => this.feedItems = feedItems != null ? feedItems : []);
+    }
+
+    test() {
+        this.feedProvider.refreshFeed().then(feedItems => this.feedItems = feedItems != null ? feedItems : []);
+    }
+
+    showItemDetails(item: FeedItemModel) {
+        this.navCtrl.push('FeedDetailPage', { item });
+    }
 
 }
