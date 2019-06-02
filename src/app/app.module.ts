88d32e49366d67e965f5b5ee720946a9334b1170
César Javier García - Páramo <cjgarcia@krama.es>
2019-06-02 13:28:00 +0200

    Configuración de idioma español

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { IonicStorageModule } from '@ionic/storage';
+import { registerLocaleData } from '@angular/common';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { FeedProvider } from '../providers/feed/feed.provider';
+
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+registerLocaleData(localeEs, localeEsExtra);
 
 @NgModule({
     declarations: [
@@ -26,8 +31,9 @@ import { FeedProvider } from '../providers/feed/feed.provider';
     providers: [
         StatusBar,
         SplashScreen,
+        FeedProvider,
         { provide: ErrorHandler, useClass: IonicErrorHandler },
-        FeedProvider
+        { provide: LOCALE_ID, useValue: 'es_ES'}
     ]
 })
 export class AppModule { }
